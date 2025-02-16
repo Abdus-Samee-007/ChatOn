eb1f2e4db6a9edb95a0147e446aa02b445d9a67d
Abdus Samee <abdus007samee@gmail.com>
2025-02-16 19:11:06 +0530

    Basic backend setup with routes

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = (req, res) => {
+    res.send('Signup Route');
+}
+
+export const login = (req, res) => {
+    res.send('Login Route');
+}
+
+export const logout = (req, res) => {
+    res.send('Logout Route');
+}
+
+export const forgetPassword = (req, res) => {
+    res.send('Forget Password Route');
+}
