f980c35f36a0b099e6707e32157cdea0530b6ce6
Abdus Samee <abdus007samee@gmail.com>
2025-02-17 21:56:38 +0530

    Fixed DB connection Error

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,9 +8,20 @@ const app=express();
 dotenv.config()
 const PORT= process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.use("/api/auth",authRoutes);
 
 app.listen(PORT,()=>{
      console.log(`Server is running on port ${PORT}`);
-     connectDB
+     connectDB();
+     
 });
+
+// connectDB().then(()=>{
+//      app.listen(PORT,()=>{
+//           console.log(`Server is running on port ${PORT}`);
+          
+//      })
+// }
+// )
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -3,21 +3,21 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
     },
     password: {
         type: String,
         required: true,
-        minLength: 6
+        minLength: 6,
     },
    profilePic: {
         type: String,
-        default: ""
+        default: "",
     },
  },
   {
