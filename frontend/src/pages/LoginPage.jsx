6391d7bfcec0807fd5f32cbb09e6c6623d9aee30
Abdus Samee <abdus007samee@gmail.com>
2025-02-23 19:10:11 +0530

    Add initial frontend setup and defined routing pages

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+      This is the Login Page
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUpPage = () => {
+  return (
+    <div>
+      This is the SignUp Page
+    </div>
+  )
+}
+
+export default SignUpPage
