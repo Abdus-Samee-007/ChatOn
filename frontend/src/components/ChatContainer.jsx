3ee980ece59473749ea0655fe3d8e58852e0b05c
Abdus Samee <abdus007samee@gmail.com>
2025-03-08 00:10:50 +0530

    Show message time improved

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -5,6 +5,7 @@ import ChatHeader from './ChatHeader'
 import MessageInput from './MessageInput'
 import MessageSkelton from './skeletons/MessageSkeleton'
 import { useAuthStore } from '../store/useAuthStore'
+import { formatMessageTime } from '../lib/utils'
 
 const ChatContainer = () => {
   const {messages, getMessages, isMessagesLoading, selectedUser} = useChatStore()
@@ -51,7 +52,7 @@ const ChatContainer = () => {
             <div className="chat-header mb-1">
               Abd
               <time className="text-xs opacity-50 ml-1">
-                {message.createdAt}
+                {formatMessageTime(message.createdAt)}
               </time>
             </div>
             <div className="chat-bubble flex flex-col">
