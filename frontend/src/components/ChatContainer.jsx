beb806376618f3e13944e3b98dd0cf4b6e062e64
Abdus Samee <abdus007samee@gmail.com>
2025-03-08 02:49:28 +0530

    Added auto-scrolling to ChatContainer

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect,useRef } from 'react'
 import {useChatStore} from '../store/useChatStore'
 
 import ChatHeader from './ChatHeader'
@@ -10,6 +10,7 @@ import { formatMessageTime } from '../lib/utils'
 const ChatContainer = () => {
   const {messages, getMessages, isMessagesLoading, selectedUser,subscribeToMessages, unsubscribeFromMessages } = useChatStore()
   const {authUser} = useAuthStore()
+  const messageEndRef = useRef(null)
 
   useEffect(() => {
     getMessages(selectedUser._id)
@@ -17,6 +18,12 @@ const ChatContainer = () => {
     return () =>  unsubscribeFromMessages();
 
   }, [selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages])
+
+  useEffect(() => {
+    if(messageEndRef.current && messages){
+      messageEndRef.current.scrollIntoView({behavior:'smooth'})
+    }
+  }, [messages])
 
   if (isMessagesLoading) {
     return (
@@ -35,6 +42,7 @@ const ChatContainer = () => {
         {messages.map((message) => (
           <div key={message._id}
             className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+            ref={messageEndRef}
           >
             <div className=" chat-image avatar">
               <div className="size-10 rounded-full border">
@@ -49,7 +57,6 @@ const ChatContainer = () => {
               </div>
             </div>
             <div className="chat-header mb-1">
-              Abd
               <time className="text-xs opacity-50 ml-1">
                 {formatMessageTime(message.createdAt)}
               </time>
